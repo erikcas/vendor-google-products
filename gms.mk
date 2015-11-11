414862a8a96442b91f9300daa87786cf568ead66
Erik Castricum <erikcas1972@gmail.com>
2015-11-11 13:41:54 -0500

    initial

---
+++ gms.mk
@@ -0,0 +1,4 @@
+$(call inherit-product, vendor/google/products/32bit/google-partial.mk)
+$(call inherit-product, vendor/google/products/32bit/vendor-google-apps.mk)
+$(call inherit-product, vendor/google/products/64bit/google-partial.mk)
+$(call inherit-product, vendor/google/products/64bit/vendor-google-apps.mk)
